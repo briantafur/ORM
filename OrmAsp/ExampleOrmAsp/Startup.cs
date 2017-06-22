6bbcfe0f4fab50b85f66cb36c6d2928f4b5bd182
Brian David Tafur Londoño <brian.tafur@yuxiglobal.com>
2017-06-22 11:33:45 -0500

    commit

---
+++ OrmAsp/ExampleOrmAsp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExampleOrmAsp.Startup))]
+namespace ExampleOrmAsp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
